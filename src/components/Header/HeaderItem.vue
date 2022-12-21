25315a4f34d5bc3e23dc7f1f6b2f0e4332be99dc
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-12-21 16:26:17 +0100

    feat(header): add navbar and logo

---
+++ src/components/Header/HeaderItem.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import logo from "../../assets/logo.png";
+</script>
+<template>
+  <header class="header-item">
+    <img :src="logo" alt="heysun logo" class="logo ms-4" />
+    <nav class="navbar">
+      <ul class="navbar-nav">
+        <li>
+          <router-link to="/" class="nav-link me-5">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/about" class="nav-link me-4">About</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+<style scoped src="./HeaderItem.css"></style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>home</main>
+  <main>
+    <p>about</p>
+  </main>
 </template>
