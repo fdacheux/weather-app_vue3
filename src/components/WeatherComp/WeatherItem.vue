9bd0558d73b2bc5b3037e78e73d6466057d0db2c
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-12-21 22:25:10 +0100

    feat(weather comp): inject data from api call

--- src/components/WeatherComp/WeatherItem.vue
+++ src/components/WeatherComp/WeatherItem.vue
@@ -1,34 +1,46 @@
 <script setup lang="ts">
 import { getWeather } from "@/stores/getWeather";
 import { ref } from "vue";
+import type weatherModel from "../../models/weatherObject.model";
 const request = ref({
   cityName: "",
 });
-const goWeather = () => {
-  console.log("This is fine");
+let weather: weatherModel | undefined = undefined;
+const goWeather = async () => {
+  weather = await getWeather(request.value.cityName);
+  request.value.cityName = "";
 };
-const weather = getWeather(request.value.cityName);
 </script>
 
 <template>
   <div class="container">
-    <h2 class="my-4">A Weather app using vue3</h2>
+    <h2 class="my-4">What's the weather like in your city ?</h2>
     <div class="form-group mb-5">
-      <label for="location">Please, enter a city name</label>
+      <label for="location" class="label mb-3"
+        >Please, enter a city name :</label
+      >
       <input
         id="location"
         type="text"
         class="form-control"
         v-model="request.cityName"
-        @keypress="goWeather"
+        @keypress.enter="goWeather"
       />
     </div>
-    <div class="w-75 m-auto">
-      <h3 class="text-center">Location : dynamic name</h3>
+    <div class="w-75 m-auto" v-if="weather">
+      <h3 class="text-center mb-4">Location : {{ weather.name }}</h3>
       <div class="card text-center p-5">
-        <p class="text-display">Temperature : 20°</p>
-        <p class="text-display">Weather : Cloudy</p>
+        <div class="wrapper">
+          <p class="text-display">
+            Temperature : {{ weather.main.temp.toFixed() }}°C
+          </p>
+          <p class="text-display">
+            Weather : {{ weather.weather[0].description }}
+          </p>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped src="./WeatherItem.css"></style>
--- src/stores/getWeather.ts
+++ src/stores/getWeather.ts
@@ -1,53 +1,11 @@
-interface IWeatherObject {
-  coord: {
-    lon: number;
-    lat: number;
-  };
-  weather: [
-    {
-      id: number;
-      main: string;
-      description: string;
-      icon: string;
-    }
-  ];
-  base: string;
-  main: {
-    temp: number;
-    feels_like: number;
-    temp_min: number;
-    temp_max: number;
-    pressure: number;
-    humidity: number;
-  };
-  visibility: number;
-  wind: {
-    speed: number;
-    deg: number;
-  };
-  clouds: {
-    all: number;
-  };
-  dt: number;
-  sys: {
-    type: number;
-    id: number;
-    country: string;
-    sunrise: number;
-    sunset: number;
-  };
-  timezone: number;
-  id: number;
-  name: string;
-  cod: number;
-}
+import type weatherModel from "../models/weatherObject.model";
 
-const api_key = import.meta.env.API_KEY;
+const api_key = import.meta.env.VITE_API_KEY;
 
-export const getWeather = async (cityName: string): Promise<IWeatherObject> => {
+export const getWeather = async (cityName: string): Promise<weatherModel> => {
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api_key}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${api_key}`
     );
     const state = await response.json();
     console.log(state);
