08a370cb35789456cb0fa36d930af6287444d10f
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-12-21 17:21:56 +0100

    feat(weathercomp): create non dynamic weather component

---
+++ src/components/WeatherComp/WeatherItem.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const request = ref({
+  cityName: "",
+});
+const goWeather = () => {
+  console.log("This is fine");
+};
+</script>
+
+<template>
+  <div class="container">
+    <h2 class="my-4">A Weather app using vue3</h2>
+    <div class="form-group mb-5">
+      <label for="location">Please, enter a city name</label>
+      <input
+        id="location"
+        type="text"
+        class="form-control"
+        v-model="request.cityName"
+        @keypress="goWeather"
+      />
+    </div>
+    <div class="w-75 m-auto">
+      <h3 class="text-center">Location : dynamic name</h3>
+      <div class="card text-center p-5">
+        <p class="text-display">Temperature : 20°</p>
+        <p class="text-display">Weather : Cloudy</p>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import WeatherItem from "../components/WeatherComp/WeatherItem.vue";
+</script>
 
 <template>
-  <main>home</main>
+  <main>
+    <h1 class="display-5">Hey Sun ! - A sunny weather app</h1>
+    <WeatherItem />
+  </main>
 </template>
