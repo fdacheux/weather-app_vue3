787e231f83d06725fd197d00f844b825eee8a74a
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-12-22 16:51:38 +0100

    chore(all): set up firebase

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { firebaseApp } from "./firebaseConfig.js";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style/main.css";
