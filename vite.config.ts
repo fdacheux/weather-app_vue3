637bea4efea6e2733dd26300477894c87b2951b4
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-12-22 12:07:54 +0100

    fix(vue and vite config): fixing public url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/heysun-weather-app/",
+  base: "/weather-app_vue3/",
   plugins: [vue()],
   resolve: {
     alias: {
