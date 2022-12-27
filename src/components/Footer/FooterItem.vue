2b3bc5bcae2557307d95b416f060266f7de0a6f4
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-12-27 14:37:59 +0100

    feat(navigation): improve navigation add favicon

--- src/components/Footer/FooterItem.vue
+++ src/components/Footer/FooterItem.vue
@@ -4,7 +4,11 @@
   <footer>
     <div class="wrapper">
       More of my projects on :
-      <a href="https://github.com/fdacheux"
+      <a
+        href="https://github.com/fdacheux"
+        aria-label="Visit my github account"
+        target="_blank"
+        rel="noopener noreferrer"
         ><font-awesome-icon icon="fa-brands fa-github"
       /></a>
     </div>
